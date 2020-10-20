03c6039bdc1eb11b7bed97d60b8715682955498d
Rifki Mubarok <177006078@student.unsil.ac.id>
2020-10-20 21:06:11 +0700

    Revert "hehe"
    This reverts commit 3ef9355b3d8e5aca7c7e9d131e090a99453e1882.

--- application/views/sesi/edit_potongan.php
+++ application/views/sesi/edit_potongan.php
@@ -7,22 +7,19 @@
             <h1 class="h3 mb-2 text-gray-800">Bayar Piutang</h1>
             <p class="mb-2">Bayar piutang crew</p>
 
-            <!-- Form Konfigurasi Potongan Harga -->
+            <!-- DataTales Example -->
             <div class="card shadow mb-4">
                 <div class="card-body">
-                    <form class="edit-form" action="<?= base_url('crew/potongan/') . $crew_id['id_crew']; ?>"
-                        method="post">
+                    <form class="edit-form" action="<?= base_url('crew/potongan/') . $crew_id['id_crew']; ?>" method="post">
                         <div class="form-group">
                             <input type="hidden" name="id" id="id" value="<?= $crew_id['id_crew'] ?>">
                             <label class="col-form-label" for="nama">Nama</label>
-                            <input type="text" class="form-control form-control-user" id="nama" name="nama"
-                                value="<?= $crew_id['nama'] ?>">
+                            <input type="text" class="form-control form-control-user" id="nama" name="nama" value="<?= $crew_id['nama'] ?>">
                             <?= form_error('nama', '<small class="text-danger pl-3">', '</small>'); ?>
                         </div>
                         <div class="form-group">
                             <label class="col-form-label" for="potongan">Piutang</label>
-                            <input type="number" class="form-control form-control-user" id="potongan" placeholder="0"
-                                name="potongan">
+                            <input type="number" class="form-control form-control-user" id="potongan" placeholder="0" name="potongan">
                             <?= form_error('potongan', '<small class="text-danger pl-3">', '</small>'); ?>
                         </div>
                         <div class="form-group">
